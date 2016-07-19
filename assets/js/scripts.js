cdf1f2f3a84a5bce0ac6e076619f6154938e41f7
Slesarev Sergei <slesarev071290@gmail.com>
2016-07-19 17:07:37 +0300

    template\+checkout\+markup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,47 +5,49 @@ $(function() {
       isTop: false,
       el: $('.navbar')
     },
-    header = $('.header');
-  headerNavbar = $('.header__navbar');
+    mainScroll = {
+      isTop: false,
+    },
+    header = $('.header'),
+    headerNavbar = $('.header__navbar'),
+    scroll = $('.scrollToTop');
 
-  // if (body.hasClass('home')) {
-    //  Slick Slider
-    $('.slider').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 3,
-      responsive: [{
-        breakpoint: 1268,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          arrows: false
-        }
-      }, {
-        breakpoint: 1070,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          arrows: false
-        }
-      }, {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false
-        }
-      }, {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false
-        }
-      }]
-    });
-  // }
+  //  Slick Slider
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [{
+      breakpoint: 1268,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        arrows: false
+      }
+    }, {
+      breakpoint: 1070,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false
+      }
+    }, {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+      }
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }]
+  });
 
   //  Input, Textarea
   $("input,textarea").each(function() {
@@ -68,6 +70,7 @@ $(function() {
 
   $(window).resize(function() {
     runMenuBehaviour();
+    // runScroll();
   });
 
   function runMenuBehaviour() {
@@ -96,9 +99,53 @@ $(function() {
 
   runMenuBehaviour();
 
+  //Check to see if the window is top if not then display button
+  
+  // function runScroll() {
+  //   if ($(window).width() > 768) {
+  //     if (mainScroll.isTop) return;
+  //     mainScroll.isTop = true;
+  //     toggleScroll({
+  //       isTop: true
+  //     })
+  //   } else {
+  //     if (!mainScroll.isTop) return;
+  //     mainScroll.isTop = false;
+  //     toggleScroll({
+  //       isTop: false
+  //     })
+  //   }
+  // }
+
+  // function toggleScroll(direction) {
+  //   if (direction.isTop == true) {
+  //     console.log('show!');
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 2200) {
+          scroll.fadeIn();
+        } else {
+          scroll.fadeOut();
+        }
+      });
+  //   } else {
+  //     console.log('hide!');
+  //     scroll.hide();
+  //   }
+  // }
+
+  // runScroll();
+
+  //Click event to scroll to top
+  scroll.click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+
 });
 
-//preloader
+//Spiner
 $(window).load(function() {
 
   var body = $('body');
